2d133bfa7bffda31136a05d2aa0493cb51bd2545
Eric Madureira <ericmadureira@users.noreply.github.com>
2022-07-26 17:24:18 -0300

    mock items for testing

---
+++ src/mockItems.ts
@@ -0,0 +1,22 @@
+import { Item } from "./types/Item"
+
+const mockItemList: Item[] = [
+    {
+        name: 'Pão de forma Visconti',
+        amount: 1,
+        expirationDate: '11/08/2022',
+        price: 5.50,
+        category: 'food',
+        weight: 400
+    },
+    {
+        name: 'Mussarela Litoral',
+        amount: 1,
+        expirationDate: '11/08/2022',
+        price: 5.50,
+        category: 'food',
+        weight: 300
+    }
+]
+
+export default mockItemList
