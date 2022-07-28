66d40b83a016adc7109122b7fb13cfe75c777ad1
Eric Madureira <ericmadureira@users.noreply.github.com>
2022-07-28 15:14:14 -0300

    mock add button
    adds a add item button with mock behavior

--- src/pages/ItemManagementPage.tsx
+++ src/pages/ItemManagementPage.tsx
@@ -8,14 +8,28 @@ import './ItemManagementPage.css'
 
 const ItemManagementPage = () => {
     const [itemList, setItemList] = useState<Item[]>(mockItemList)
+    const handleAddNewItem = () => {
+        let newList = [...itemList]
+        newList.push({
+            id: 'hudbhfjd',
+            name: 'novo item',
+            amount: 1,
+            expirationDate: ',22/12/2022',
+            price: 44,
+            category: 'generic',
+            weight: 200,
+        })
+        setItemList(newList)
+    }
+
     return (
         <div className='item-mgmt-page__page-wrapper'>
             <h1>Lista de itens</h1>
-            <button>Add</button>
+            <button onClick={handleAddNewItem}>Add</button>
             <div className='item-mgmt-page__list-wrapper'>
                 {
                     itemList.map(item => (
-                        <div className='item-mgmt-page__item-wrapper'>
+                        <div key={item.id} className='item-mgmt-page__item-wrapper'>
                             <div>name: {item.name}</div>
                             <div>amount: {item.amount}</div>
                             <div>expirationDate: {item.expirationDate}</div>
