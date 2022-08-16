135a7538867819d6bba150cbef6a06f79dae4423
Eric Madureira <ericmadureira@users.noreply.github.com>
2022-08-16 15:34:58 -0300

    extend item interface
    Extend Item interface to include creation params, and export them.

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -1,15 +1,17 @@
-interface ExpirationDate {
+export interface ExpirationDate {
     seconds: number
     nanoseconds: number
 }
 
-export interface Item {
-    id: string // Provided by firebase on creation
-
+export interface ItemCreationParams {
     name: string
     amount: number
     expirationDate: ExpirationDate
     price: number // R$
     category: string
     weight: number // grams
+}
+
+export interface Item extends ItemCreationParams {
+    id: string // Provided by firebase on creation
 }
