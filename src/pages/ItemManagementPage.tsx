1c6e9cd90d699d498ebefa7b9f9a97771dacf017
Eric Madureira <ericmadureira@users.noreply.github.com>
2022-12-29 15:12:24 -0300

    item mgmt data grid
    Adds responsive data grid to display items and other features like filter, sorting and toggle columns.

--- src/pages/ItemManagementPage.tsx
+++ src/pages/ItemManagementPage.tsx
@@ -2,14 +2,35 @@ import React, { useState, useEffect } from 'react'
 import { collection, query, onSnapshot, addDoc } from 'firebase/firestore'
 import { firebaseApp } from '../firebase'
 import { useNavigate } from 'react-router-dom'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
 
 import AddItem from '../components/AddItem'
-import ItemRow from '../components/ItemRow'
+import { currencyFormatOptions, expirationDateFormatter } from '../helpers/formatting'
 import { Item, ItemCreationParams } from '../types/Item'
 
 import './ItemManagementPage.css'
 
 
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', flex: 1, hide: true },
+    { field: 'name', headerName: 'Nome', flex: 1 },
+    { field: 'amount', headerName: 'Quantidade', type: 'number', flex: 1 },
+    {
+        field: 'expirationDate',
+        headerName: 'Validade',
+        type: 'date',
+        flex: 1,
+        valueFormatter: (params) => expirationDateFormatter(params.value.seconds) },
+    {
+        field: 'price',
+        headerName: 'Preço',
+        type: 'number',
+        flex: 1,
+        valueFormatter: (params) => params.value.toLocaleString('pt-BR', currencyFormatOptions)
+    },
+    { field: 'category', headerName: 'Categoria', flex: 1 },
+    { field: 'weight', headerName: 'Peso (g | ml)', type: 'number', flex: 1 },
+]
 
 const ItemManagementPage = () => {
     const [itemList, setItemList] = useState<Item[]>([])
@@ -54,12 +75,16 @@ const ItemManagementPage = () => {
                     placeholder='Filtrar...'
                 />
             </div>
-            <div className='item-mgmt-page__list-wrapper'>
-                {
-                    itemList
-                        .filter((item: Item) => item.name.toLowerCase().includes(searchInput))
-                        .map((item: Item) => <ItemRow key={item.id} item={item} />)
-                }
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={
+                        itemList.filter((item: Item) => item.name.toLowerCase().includes(searchInput))
+                    }
+                    columns={columns}
+                    pageSize={50}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
             </div>
         </div>
     )
