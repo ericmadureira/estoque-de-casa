c9cea928c0a27fd4f086b303de0a2ea99f382c77
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-01-14 19:22:55 -0300

    shopping list v1
    Adds 1st version of shopping list page with items in memory.

--- src/pages/ShoppingList/ShoppingListPage.tsx
+++ src/pages/ShoppingList/ShoppingListPage.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Item } from '../../types/Item'
 
 import './ShoppingListPage.css'
 
 const ShoppingListPage = () => {
+    const [shoppingList, setShoppingList] = useState<Item[]>([])
+    const [itemName, setItemName] = useState('')
+    const addShoppingListItem = () => {
+        setShoppingList([...shoppingList, {
+            id: itemName,
+            name: itemName,
+            amount: 1,
+            expirationDate: { seconds: 1, nanoseconds: 1 },
+            price: 1,
+            category: 'default',
+            weight: 1,
+        }])
+    }
+
     return (
         <div className='shopping-list-page__page-wrapper'>
-            This is the shopping list page!
+            <h1>Shopping List page</h1>
+
+            <div className='shopping-list-page__inputs-wrapper'>
+                <input type='text' value={itemName} onChange={(event) => setItemName(event.target.value)} />
+            </div>
+
+            <button
+                style={{ width: '80px' }}
+                onClick={addShoppingListItem}
+            >
+                Adicionar item
+            </button>
+
+            <div className='shopping-list-page__list-wrapper'>
+                <ol>
+                    { shoppingList.map(item =>
+                        <li key={item.name}>
+                            <span style={{ marginRight: '10px' }}>id: {item.id}</span>
+                            <span style={{ marginRight: '10px' }}>{item.name}</span>
+                            <button onClick={() => setShoppingList(shoppingList.filter(item => item.name !== itemName))}> X </button>
+                        </li>
+                    )}
+                </ol>
+            </div>
         </div>
     )
 }
