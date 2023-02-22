920eb33dfddbbcf9014351f03eb088d2125ed046
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-02-22 17:02:12 -0300

    fix firebase imports
    Separates firebase imports in correct namespaces.

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import { getAuth, initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
