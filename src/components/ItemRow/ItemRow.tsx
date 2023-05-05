383a635f56fa4df846975a069e2144cbb957d44b
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-05-05 11:38:00 -0300

    update item form
    Adds v1 of update item form, with a toogle between view and edit modes.

--- src/components/ItemRow/ItemRow.tsx
+++ src/components/ItemRow/ItemRow.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { currencyFormatOptions, expirationDateFormatter } from '../../helpers/formatting'
+import React, { useState } from 'react'
 
+import { currencyFormatOptions, expirationDateFormatter } from '../../helpers/formatting'
 import { Item } from '../../types/Item'
 
 import './ItemRow.css'
@@ -8,25 +8,52 @@ import './ItemRow.css'
 interface ItemRowProps {
     item: Item
 }
+enum ViewMode {
+    View = 'view',
+    Edit = 'edit'
+}
 
 const ItemRow = ({ item }: ItemRowProps) => {
-    const { name, amount, expirationDate, price, weight } = item
+    const { amount, name, expirationDate, price, weight, ean } = item
+
+    const handleClickEdit = () => {
+        setViewMode((viewMode === ViewMode.View) ? ViewMode.Edit : ViewMode.View)
+    }
+
+    const [viewMode, setViewMode] = useState(ViewMode.View)
+    const [editFormItemAmount, setEditFormItemAmount] = useState<number>(amount)
+    const [editFormItemName, setEditFormItemName] = useState<string>(name)
+    const [editFormItemExpirationDate, setEditFormItemExpirationDate] = useState<string>(expirationDateFormatter(expirationDate.seconds))
+    const [editFormItemPrice, setEditFormItemPrice] = useState<number>(price)
+    // const [editFormItemcategory, setEditFormItemcategory] = useState<string>('')
+    const [editFormItemWeight, setEditFormItemWeight] = useState<number>(weight)
+    const [editFormItemEAN, setEditFormItemEAN] = useState<string>(ean)
+
     return (
         <div className='item-row__wrapper'>
-            <span className='item-row__amount'>{amount}</span>
-            <b>{name}</b>
-
-            {/* validade será escondida */}
-            <span>
+            { (viewMode === ViewMode.View) ?
+                // View mode
                 <>
-                    Val.: {expirationDateFormatter(expirationDate.seconds)}
+                    <span className='item-row__amount'>{amount}</span>
+                    <b>{name}</b>
+                    <span>Val.: {expirationDateFormatter(expirationDate.seconds)}</span>
+                    <span>{price.toLocaleString('pt-BR', currencyFormatOptions)}</span>
+                    <span>{weight} g</span>
+                    <span>{ean}</span>
+                    <button onClick={handleClickEdit}><i className='fa-solid fa-pen-to-square' /></button>
+                </> :
+                // Edit mode
+                <>
+                    <input onChange={(e) => setEditFormItemAmount(Number(e.target.value))} value={editFormItemAmount} type='number' min={0} />
+                    <input onChange={(e) => setEditFormItemName(e.target.value)} value={editFormItemName} type='text' />
+                    <input onChange={(e) => setEditFormItemExpirationDate(e.target.value)} value={editFormItemExpirationDate} type='datetime-local' />
+                    <input onChange={(e) => setEditFormItemPrice(Number(e.target.value))} value={editFormItemPrice} type='number' min={0} />
+                    {/* <input onChange={(e) => setFormItemcategory(e.target.value)} value={formItemcategory} type='text' /> */}
+                    <input onChange={(e) => setEditFormItemWeight(Number(e.target.value))} value={editFormItemWeight} type='number' min={0} />
+                    <input onChange={(e) => setEditFormItemEAN(e.target.value)} value={editFormItemEAN} type='text' />
+                    <button onClick={handleClickEdit}>SAVE</button>
                 </>
-            </span>
-
-            {/* preço será escondido */}
-            <span>{price.toLocaleString('pt-BR', currencyFormatOptions)}</span>
-            <span>{weight} g</span>
-            <span><i className='fa-solid fa-flask-vial' /></span>
+            }
         </div>
     )
 }
