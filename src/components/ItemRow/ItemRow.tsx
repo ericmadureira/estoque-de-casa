a31b922f615945046cc57e0bd2a3c38d3c15fca7
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-05-11 08:22:18 -0300

    change formatting in edit form
    Changes expiration date formatting to yyyy/mm/dd.

--- src/components/ItemRow/ItemRow.tsx
+++ src/components/ItemRow/ItemRow.tsx
@@ -35,14 +35,17 @@ const ItemRow = ({ item }: ItemRowProps) => {
     const [viewMode, setViewMode] = useState(ViewMode.View)
     const [editFormItemAmount, setEditFormItemAmount] = useState<number>(amount)
     const [editFormItemName, setEditFormItemName] = useState<string>(name)
-    const [editFormItemExpirationDate, setEditFormItemExpirationDate] = useState<string>(serializeDate(expirationDate.seconds))
+    const [editFormItemExpirationDate, setEditFormItemExpirationDate] = useState<string>(
+        serializeDate(expirationDate.seconds)
+            .toLocaleDateString('en-US')
+    )
     const [editFormItemPrice, setEditFormItemPrice] = useState<number>(price)
     const [editFormItemcategory, setEditFormItemcategory] = useState<string>(category)
     const [editFormItemWeight, setEditFormItemWeight] = useState<number>(weight)
     const [editFormItemEAN, setEditFormItemEAN] = useState<string>(ean)
 
     const formattedPrice = price.toLocaleString('pt-BR', currencyFormatOptions)
-    const formattedExpirationDate = serializeDate(expirationDate.seconds)
+    const formattedExpirationDate = serializeDate(expirationDate.seconds).toLocaleDateString('pt-BR')
 
     return (
         <div className='item-row__wrapper'>
@@ -62,7 +65,7 @@ const ItemRow = ({ item }: ItemRowProps) => {
                 <>
                     <input onChange={(e) => setEditFormItemAmount(Number(e.target.value))} value={editFormItemAmount} type='text' />
                     <input onChange={(e) => setEditFormItemName(e.target.value)} value={editFormItemName} type='text' />
-                    <input onChange={(e) => setEditFormItemExpirationDate(e.target.value)} value={editFormItemExpirationDate} type='datetime-local' />
+                    <input onChange={(e) => setEditFormItemExpirationDate(e.target.value)} value={editFormItemExpirationDate} type='date' />
                     <input onChange={(e) => setEditFormItemPrice(Number(e.target.value))} value={editFormItemPrice} type='text' />
                     <input onChange={(e) => setEditFormItemcategory(e.target.value)} value={editFormItemcategory} type='text' />
                     <input onChange={(e) => setEditFormItemWeight(Number(e.target.value))} value={editFormItemWeight} type='text' />
--- src/helpers/formatting.ts
+++ src/helpers/formatting.ts
@@ -1,5 +1,5 @@
 export const currencyFormatOptions = { style: 'currency', currency: 'BRL' }
-export const serializeDate = (seconds: number) => new Date(seconds*1000).toLocaleDateString('pt-BR')
+export const serializeDate = (seconds: number) => new Date(seconds*1000)
 export const deserializeDate = (dateTime: string) => {
   return {
     seconds: Math.round(new Date(dateTime).getTime()/1000),
