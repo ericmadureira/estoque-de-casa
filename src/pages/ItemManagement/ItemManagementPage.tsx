a9f41a2719bde0fcaf3a28641477f6f4c965dbf2
Eric Madureira <ericmadu416@gmail.com>
2023-12-14 16:40:38 -0300

    feat: change add item logic on management page

--- src/pages/ItemManagement/ItemManagementPage.tsx
+++ src/pages/ItemManagement/ItemManagementPage.tsx
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import AddItem from '../../components/AddItem'
+import AddItemModal from '../../components/AddItemModal'
+import EditItemModal from '../../components/EditItemModal'
 import ItemRow from '../../components/ItemRow/ItemRow'
 import { addNewItem, getAllItems } from '../../services/item-data'
 import { Item, ItemCreationParams } from '../../types/Item'
 
 import './ItemManagementPage.css'
-import ItemEditModal from '../../components/ItemEditModal'
 
+const EMPTY_ITEM: Item = { id: '', amount: 0, name: '', expirationDate: { seconds: 0, nanoseconds: 0 },
+    price: 0, category: '', weight: 0, ean: '' }
 
 const ItemManagementPage = () => {
     // State
     const [itemList, setItemList] = useState<Item[]>([])
     const [searchInput, setSearchInput] = useState<string>('')
     const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false)
-    const [selectedItem, setSelectedItem] = useState<Item>(
-        { id: '', amount: 0, name: '', expirationDate: { seconds: 0, nanoseconds: 0 }, price: 0, category: '', weight: 0, ean: '' })
+    const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false)
+    const [selectedItem, setSelectedItem] = useState<Item>(EMPTY_ITEM)
     const navigate = useNavigate()
 
     // Methods
@@ -24,12 +26,15 @@ const ItemManagementPage = () => {
         await addNewItem(itemCreationParams)
         navigate('/')
     }
-
     const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         event.preventDefault()
         setSearchInput(event.target.value)
     }
+    const handleClickAddItem = () => {
+        setIsAddModalOpen(true)
+    }
 
+    // Effects
     useEffect(() => {
         // Fetches all items from db/firebase.
         getAllItems().then(items => setItemList(items))
@@ -38,13 +43,14 @@ const ItemManagementPage = () => {
     return (
         <div className='container'>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <AddItem handleAddNewItem={handleAddNewItem} />
+                <button style={{ marginRight: 8, width: 60 }}><i className='fa-solid fa-barcode' /></button>
+                <button style={{ marginRight: 8, width: 60 }} onClick={handleClickAddItem}><i className='fa-solid fa-plus' /></button>
                 <input
                     type="text"
                     value={searchInput}
                     onChange={handleSearchInputChange}
                     placeholder='Filtrar...'
-                    style={{ marginBottom: 0 }}
+                    style={{ marginBottom: 0, flexGrow: 4 }}
                 />
             </div>
             <div className='list-header'>
@@ -52,21 +58,25 @@ const ItemManagementPage = () => {
                 <span style={{ flexGrow: 4 }}>Nome</span>
             </div>
             <div style={{ width: '100%' }}>
-                {
-                    itemList.filter((item: Item) => item.name.toLowerCase().includes(searchInput)).map(item => {
-                        return (
-                            <ItemRow
-                                item={item}
-                                key={item.id}
-                                setIsEditModalOpen={setIsEditModalOpen}
-                                setSelectedItem={setSelectedItem}
-                            />
-                        )
-                    })
-                }
+                { itemList.filter((item: Item) => item.name.toLowerCase().includes(searchInput)).map(item => {
+                    return (
+                        <ItemRow
+                            item={item}
+                            key={item.id}
+                            setIsEditModalOpen={setIsEditModalOpen}
+                            setSelectedItem={setSelectedItem}
+                        />
+                    )
+                })}
             </div>
+            { isAddModalOpen &&
+                <AddItemModal
+                    handleAddNewItem={handleAddNewItem}
+                    setIsAddModalOpen={setIsAddModalOpen}
+                />
+            }
             { isEditModalOpen &&
-                <ItemEditModal
+                <EditItemModal
                     selectedItem={selectedItem}
                     setIsEditModalOpen={setIsEditModalOpen}
                 />
