e1a374bea3b36a1f80f0a3637a5c9dd97d949eb7
Eric Madureira <ericmadu416@gmail.com>
2023-12-15 21:23:28 -0300

    feat: move edit item logic to parent component

--- src/components/EditItemModal.tsx
+++ src/components/EditItemModal.tsx
@@ -2,19 +2,19 @@ import React, { useState } from 'react'
 
 import { deserializeDate, serializeDate } from '../helpers/formatting'
 import ModalInput from './ModalInput'
-import { updateItem } from '../services/item-data'
-import { Item } from '../types/Item'
+import { Item, ItemUpdateParams } from '../types/Item'
 
 interface EditItemModalProps {
+  handleUpdateItem: (itemUpdateParams: ItemUpdateParams) => Promise<void>
   selectedItem: Item
   setIsEditModalOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
-const EditItemModal = ({ selectedItem, setIsEditModalOpen }: EditItemModalProps): JSX.Element => {
+const EditItemModal = ({ handleUpdateItem, selectedItem, setIsEditModalOpen }: EditItemModalProps): JSX.Element => {
   // State
   const [amount, setAmount] = useState<number>(selectedItem.amount)
   const [name, setName] = useState<string>(selectedItem.name)
   const [expirationDate, setExpirationDate] = useState<string>(
-    serializeDate(selectedItem.expirationDate.seconds).toLocaleDateString('en-US'))
+    serializeDate(selectedItem.expirationDate.seconds).toISOString().split('T')[0])
   const [price, setPrice] = useState<number>(selectedItem.price)
   const [category, setCategory] = useState<string>(selectedItem.category)
   const [weight, setWeight] = useState<number>(selectedItem.weight)
@@ -23,9 +23,8 @@ const EditItemModal = ({ selectedItem, setIsEditModalOpen }: EditItemModalProps)
   // Methods
   const handleClickCancel = () => setIsEditModalOpen(false)
   const handleClickSave = async () => {
-    await updateItem({ id: selectedItem.id, amount, name: name, expirationDate: deserializeDate(expirationDate),
+    await handleUpdateItem({ id: selectedItem.id, amount, name: name, expirationDate: deserializeDate(expirationDate),
       price: price, category: category, weight: weight, ean: EAN })
-    setIsEditModalOpen(false)
   }
 
   return (
@@ -41,7 +40,8 @@ const EditItemModal = ({ selectedItem, setIsEditModalOpen }: EditItemModalProps)
         <ModalInput label='Nome' onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setName(e.target.value) }} value={name} />
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 16 }}>
           <span>Validade: </span>
-          <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setExpirationDate(e.target.value) }} style={{ margin: '0 0 0 16px', width: 215 }} type='date' value={expirationDate} />
+          <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setExpirationDate(e.target.value) }}
+            style={{ margin: '0 0 0 16px', width: 215 }} type='date' value={expirationDate} />
         </div>
         <ModalInput label='Preço' onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setPrice(Number(e.target.value)) }} value={price} />
         <ModalInput label='Categoria' onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setCategory(e.target.value) }} value={category} />
