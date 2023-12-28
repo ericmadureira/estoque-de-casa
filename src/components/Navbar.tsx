ce7755a07171351bcef09bb7acc4c713483e9a98
Eric Madureira <ericmadu416@gmail.com>
2023-12-28 14:11:07 -0300

    feat: navbar links for pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,26 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='container'
-            style={{
-                display: 'flex', backgroundColor: '#1095C1',
-                justifyContent: 'space-between', alignItems: 'center',
-                color: 'white', padding: 16, marginBottom: 16
+        <nav className='container'
+            style={{ display: 'flex', backgroundColor: '#1095C1',
+                justifyContent: 'space-between', alignItems: 'center', marginBottom: 16
         }}>
-            <Link to='/'><span style={{ fontWeight: 'bold', color: 'white' }}>Estoque de Casa</span></Link>
-            <i className='fa-regular fa-circle-user fa-2x' />
-        </div>
+            <ul>
+                <li>
+                    <Link to='/'>
+                        <span style={{ fontWeight: 'bold', color: 'white' }}>Estoque</span>
+                    </Link>
+                </li> | 
+                <li>
+                    <Link to='/lista'>
+                        <span style={{ fontWeight: 'bold', color: 'white' }}>Lista de compras</span>
+                    </Link>
+                </li>
+            </ul>
+            <ul>
+                <li><i className='fa-regular fa-circle-user' style={{ fontSize: 24, color: 'white' }} /></li>
+            </ul>
+        </nav>
     )
 }
 
