ffc994b92b19d92db0c61ac9dd379ade9a8bfd15
Eric Madureira <ericmadu416@gmail.com>
2024-01-08 17:11:05 -0300

    feat: input format for modal

--- src/components/ModalInput.tsx
+++ src/components/ModalInput.tsx
@@ -5,12 +5,13 @@ interface ModalInputProps {
   label: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   value: string | number
+  inputFormat: 'number' | 'text' | 'date'
 }
-const ModalInput = ({ autoFocus, label, onChange, value }: ModalInputProps) => {
+const ModalInput = ({ autoFocus, inputFormat, label, onChange, value }: ModalInputProps) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 16 }}>
       <span>{label}: </span>
-      <input onChange={onChange} style={{ margin: '0 0 0 16px', width: 215 }} type='text' value={value} autoFocus={autoFocus} />
+      <input onChange={onChange} style={{ margin: '0 0 0 16px', width: 215 }} type={inputFormat} value={value} autoFocus={autoFocus} />
     </div>
   )
 }
