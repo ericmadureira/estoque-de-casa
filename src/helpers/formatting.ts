84a4ca4dbaef2d8f72a069ec4eab695db9a55000
Eric Madureira <ericmadu416@gmail.com>
2024-01-29 10:49:21 -0300

    chore: remove unused formatters

--- src/helpers/formatting.ts
+++ src/helpers/formatting.ts
@@ -1,8 +1,1 @@
 export const currencyFormatOptions = { style: 'currency', currency: 'BRL' }
-export const formatDateFromDatabase = (seconds: number) => new Date(seconds*1000).toISOString().split('T')[0]
-export const formatDateBeforeSave = (dateTime: string) => {
-  return {
-    seconds: Math.round(new Date(dateTime).getTime()/1000),
-    nanoseconds: 0,
-  }
-}
